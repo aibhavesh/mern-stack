45d3114f60431ed4e9ef1614393701f57ef54fce
bhavesh prajapat <aibhavesh27@gmail.com>
2025-02-22 01:37:55 +0530

    today with pure raeact the routhing and server work is in intermediate level

---
+++ react - project/project1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+let App = () => {
+
+    return(
+        <>
+        <h1> hello meet </h1>
+        <button className='btn btn-danger'>click me</button>
+        <p>meet dada pada kaisa tune dekha kyA</p>
+        </>
+    )
+};export default App;
+
---
+++ react - project/project1/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+const root = ReactDOM.createRoot(document.getElementById('bhavesh'));
+ let Meet = () => {
+    return(
+        <>
+    </>
+    )
+  }
+root.render(
+  <>
+  <App />
+  <h1> hello meet </h1>
+  <button className='btn btn-danger'>click me</button>
+  <Meet/>
+  </>
+);
+
+
